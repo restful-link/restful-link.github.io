53e9087e9e575a88fe33dc4feb1898afca68a185
Jesse Shawl <jesse@jesse.sh>
2020-03-20 18:10:49 -0500

    Updates

---
+++ precache-manifest.3e0bb9f48fad834f67d99f67cfa16f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2344e8923634eae38caf9a24d19f45e7",
+    "url": "/restful-link/index.html"
+  },
+  {
+    "revision": "0e671258febfc99501a9",
+    "url": "/restful-link/static/css/main.c905f33c.chunk.css"
+  },
+  {
+    "revision": "9e7da544eeb144a8aee9",
+    "url": "/restful-link/static/js/2.eb86495c.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/restful-link/static/js/2.eb86495c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e671258febfc99501a9",
+    "url": "/restful-link/static/js/main.f1d6d58f.chunk.js"
+  },
+  {
+    "revision": "93fb1e5cfa8ff5ed31fc",
+    "url": "/restful-link/static/js/runtime-main.4ddbf3f7.js"
+  }
+]);
