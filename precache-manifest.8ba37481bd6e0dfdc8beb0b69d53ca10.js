e8ec1f27ab73042dde75834e48028e33cfe06af1
Jesse Shawl <jesse@jesse.sh>
2020-03-20 18:15:38 -0500

    Updates

---
+++ precache-manifest.8ba37481bd6e0dfdc8beb0b69d53ca10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ceefb9662ff316f7ec0892976122b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e671258febfc99501a9",
+    "url": "/static/css/main.c905f33c.chunk.css"
+  },
+  {
+    "revision": "243995b711d6f5cf9d41",
+    "url": "/static/js/2.8fc0e4a5.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/static/js/2.8fc0e4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e671258febfc99501a9",
+    "url": "/static/js/main.f1d6d58f.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
